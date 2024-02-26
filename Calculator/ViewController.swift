654c7f93181de10d03280fe1148fcf8261ad82e7
Abdulkadir Akyurt <akyurt.a@gmx.de>
2024-02-26 16:32:12 +0100

    finished

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,11 +9,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+    
+    @IBOutlet weak var firstText: UITextField!
+    @IBOutlet weak var secondText: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var result = 0.0
+        
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view.
+            resultLabel.text = String(result)
+            
+        }
+        
+        @IBAction func sumClicked(_ sender: Any) {
+            if let firstNumber = Double(firstText.text!) {
+                if let secondNumber = Double(secondText.text!) {
+                    result = firstNumber + secondNumber
+                    resultLabel.text = String(result)
+                }
+            }
+        }
+        
+        
+        @IBAction func minusClicked(_ sender: Any) {
+            if let firstNumber = Double(firstText.text!) {
+                if let secondNumber = Double(secondText.text!) {
+                    result = firstNumber - secondNumber
+                    resultLabel.text = String(result)
+                }
+            }
+            
+        }
+        
+        
+        @IBAction func multiplyClicked(_ sender: Any) {
+            if let firstNumber = Double(firstText.text!) {
+                if let secondNumber = Double(secondText.text!) {
+                    result = firstNumber * secondNumber
+                    resultLabel.text = String(result)
+                }
+            }
+        }
+        
+        
+        @IBAction func divideClicked(_ sender: Any) {
+            if let firstNumber = Double(firstText.text!) {
+                if let secondNumber = Double(secondText.text!) {
+                    result = firstNumber / secondNumber
+                    resultLabel.text = String(result)
+                }
+            }
+        }
 }
 
